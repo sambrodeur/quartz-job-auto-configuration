6c918a9d5c64b89d3c1bdc4df2830405b739eefc
Samuel Brodeur <nbasam@hotmail.com>
2025-11-08 09:11:00 -0500

    Initial commit.

---
+++ src/main/java/com/koenig/example/quartz/job/auto/configuration/QuartzJobAutoConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.koenig.example.quartz.job.auto.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzJobAutoConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzJobAutoConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koenig/example/quartz/job/auto/configuration/QuartzJobAutoConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koenig.example.quartz.job.auto.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzJobAutoConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
